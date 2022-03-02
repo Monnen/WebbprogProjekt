a81a518fe4ddae696ce11f2daff3be1dc0cb6653
Katia <ka7612sv-s@student.lu.se>
2022-03-02 11:29:59 +0100

    Skapade navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlertComponent } from './alert/alert.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlertComponent
+    AlertComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
