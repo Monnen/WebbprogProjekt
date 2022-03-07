bcc37971ee3c6258bd48e9675a0d5acaaf1a9577
Katia <ka7612sv-s@student.lu.se>
2022-03-07 11:40:26 +0100

    Översatt till engelska

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { FetchDrinkByLetterService } from 'app/fetch-drink-by-letter.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projektDrinkSidan';
+  title = 'projectDrinkSite';
 
 
 searchText: string = '';
